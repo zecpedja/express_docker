4cdaaed86fb6c97ab0a9d73457cf668953600872
zecpedja <127168701+zecpedja@users.noreply.github.com>
2024-02-05 03:38:51 +0100

    redoing Dockerfile and dockerization process

--- App.js
+++ App.js
@@ -4,28 +4,27 @@ const path = require('path');
 const app = express();
 
 const port = 3000;
+
 // Serve static files from the React app
-
 app.use(express.static(path.join(__dirname, 'client/build')));
-// The "catchall" handler: for any request that doesn't
-// match one above, send back the React's index.html file.
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
-
+// API routes
 app.get('/hello', (req, res) => {
     console.log('Check your console!');
-    res.send('Check your console!');
+    res.json({ message: 'Check your console!' }); // Respond with JSON
 });
 
 app.get('/' , (req, res) => {
     console.log('hello world');
-    res.send('Hello World!');
+    res.json({ message: 'Hello World!' }); // Respond with JSON
 });
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back the React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
-
--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,15 @@
-# Use the official Node.js 14 image.
-FROM node:14
-
-# Create and change to the app directory.
+# Build stage
+FROM node:14 AS builder
 WORKDIR /usr/src/app
-
-# Copy application dependency manifests to the container image.
 COPY package*.json ./
-
-# Install dependencies.
 RUN npm install
-
-# Copy the main application.
 COPY . .
-
-# Build the React application.
 WORKDIR /usr/src/app/client
-COPY client/package*.json ./
-RUN npm install
-RUN npm run build
+RUN npm install && npm run build
 
-# Change back to the main application directory
+# Runtime stage
+FROM node:14-slim
 WORKDIR /usr/src/app
-
-# Your application's default port
+COPY --from=builder /usr/src/app .
 EXPOSE 3000
-
-# The command to run your application
 CMD [ "node", "App.js" ]
