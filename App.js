53313d79539fd9f2ff28f3e8e11ed618312522ee
zecpedja <127168701+zecpedja@users.noreply.github.com>
2024-02-05 01:51:40 +0100

    second

--- App.js
+++ App.js
@@ -7,6 +7,11 @@ app.get('/hello', (req, res) => {
     res.send('Check your console!');
 });
 
+app.get('/' , (req, res) => {
+    console.log('hello world');
+    res.send('Hello World!');
+});
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
