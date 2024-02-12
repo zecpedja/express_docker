e15c47e1fcea9b460b744a32eb486e2094769a3f
zecpedja <127168701+zecpedja@users.noreply.github.com>
2024-02-12 20:40:44 +0100

    minor change

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Your routes her
-
-
-
+// Your routes here
 
 app.get('/api/message', (req, res) => {
     res.json({ message: 'Hello from Express!' });
